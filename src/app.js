880856ee4e1b2c73366d6f0ef8ab1224a7ab8728
Dylan Plecki <dylanplecki@gmail.com>
2016-04-03 19:17:30 -0500

    Fix styling of app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-var config = require("config");
-var app = require("express")();
-var http = require("http").Server(app);
-var WebSocketServer = require("ws").Server;
-var gameserver = require("./server/gameserver");
+var config = require('config');
+var app = require('express')();
+var http = require('http').Server(app);
+var WebSocketServer = require('ws').Server;
+var gameserver = require('./server/gameserver');
 
 /**
  * Initialize Gameserver
@@ -15,12 +15,12 @@ gameserver.initialize(wss, config);
  * HTTP Server Code
  */
 
-var port = config.get("server.port");
+var port = config.get('server.port');
 
-app.get("/", function(req, res) {
-    res.sendfile("client/index.html");
+app.get('/', function(req, res) {
+    res.sendfile('client/index.html');
 });
 
 http.listen(port, function() {
-    console.log("HTTP Server Listening On *:" + port);
+    console.log('HTTP Server Listening On *:' + port);
 });
