2d3546a4135ea23b82fcb0928f2c465f046772f5
Dylan Plecki <dylanplecki@gmail.com>
2016-04-07 01:22:44 -0500

    Begin work on game engine in go

---
+++ GameEngine.go
@@ -0,0 +1,122 @@
+package spacecluster
+
+import (
+    "os"
+    "runtime"
+    "github.com/spf13/viper"
+    "github.com/valyala/fasthttp"
+    log "github.com/Sirupsen/logrus"
+)
+
+/*
+ * Game Engine Constants
+*/
+
+const configFile = "resources/config.json"
+
+/*
+ * Game Engine Base
+*/
+
+var initialized = false
+
+func init() {
+    // Load logger
+    log.SetFormatter(&log.JSONFormatter{})
+    log.SetLevel(log.InfoLevel)
+    log.SetOutput(os.Stderr)
+
+    // Load configuration
+    viper.SetConfigName("config")
+    viper.AddConfigPath("/etc/appname/")
+    viper.AddConfigPath("$HOME/.appname")
+    viper.AddConfigPath(".")
+    setDefaultViperConfig()
+    err := viper.ReadInConfig()
+    if err != nil {
+        log.Panicf("Fatal error config file: %s \n", err)
+    }
+
+    // Load log level
+    level, err := log.ParseLevel(viper.GetString("server.log_level"))
+    if err != nil {
+        log.Error("Cannot load log level from config: ", err)
+    } else {
+        log.SetLevel(level)
+    }
+
+    // Load processor max count
+    procs := viper.GetInt("server.max_procs")
+    if (procs < 0) {
+        procs = runtime.NumCPU()
+    }
+    runtime.GOMAXPROCS(procs)
+
+    // Start HTTP server
+    h := httpRequestHandler
+	if viper.GetBool("server.http.enable_compression") {
+		h = fasthttp.CompressHandler(h)
+	}
+	if err := fasthttp.ListenAndServe(viper.GetString("server.address"), h);
+    err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
+}
+
+type configuration struct {
+    addr string
+    name string
+    region string
+    maxPlayers uint32
+    tickRate uint32 // Hertz (ticks per second)
+    frameLookbackLength uint32 // Frames
+    playerKickTimeout uint32 // Seconds
+    logLevel string
+    maxProcs int
+    httpEnableCompress bool
+}
+
+func setDefaultViperConfig() {
+    viper.SetDefault("server.address", ":8080")
+    viper.SetDefault("server.name", "UNKNOWN")
+    viper.SetDefault("server.region", "UNKNOWN")
+    viper.SetDefault("server.log_level", "warn")
+    viper.SetDefault("server.max_procs", "warn")
+    viper.SetDefault("server.http.enable_compression", true)
+    viper.SetDefault("game_engine.max_players", 256)
+    viper.SetDefault("game_engine.tick_rate", 30)
+    viper.SetDefault("game_engine.frame_lookback_length", 30)
+    viper.SetDefault("game_engine.player_kick_timeout", 3)
+}
+
+func httpRequestHandler(ctx *fasthttp.RequestCtx) {
+
+}
+
+/*
+ * Game Engine Core
+*/
+
+// GameEngine exported
+var GameEngine gameEngine
+
+// GameEngine exported
+type gameEngine struct {
+    tick uint64
+    playerCount uint32
+    frames []gameFrame
+    objects []gameObject
+}
+
+// GameFrame exported
+type gameFrame struct {
+    tick uint64
+    events []GameEvent
+    updates []GameObjUpdate
+}
+
+// GameObject exported
+type gameObject struct {
+    state GameObjUpdate
+    isPlayer bool
+}
--- game_engine.go
+++
@@ -1,7 +0,0 @@
-package spacecluster
-
-import (
-    "github.com/gorilla/websocket"
-    "github.com/golang/protobuf/proto"
-    "github.com/valyala/fasthttp"
-)
