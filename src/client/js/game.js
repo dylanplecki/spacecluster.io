ac38d8900070dcfd83169e5f571c95e9249dfdb2
raghavnarain3 <raghavnarain3@gmail.com>
2016-04-23 22:02:00 -0500

    update but still server error

--- src/client/js/game.js
+++ src/client/js/game.js
@@ -2,8 +2,8 @@ var serverInfo;
 tree = new BinTree(function (a, b) {
     return a.id - b.id
 });
-main = new GameObj(Math.floor((Math.random() * 1000) + 1), 100, 100, 50, 2, 100, "dude", "main", '#' + Math.floor(Math.random() * 16777215).toString(16));
-var t;
+main = new GameObj(Math.floor((Math.random() * 1000) + 1), 100, 100, 50, 2, 100, "dude", "main", '#' + Math.floor(Math.random() * 16777215).toString(16), 0);
+var t = 0;
 
 (function () {
     var tps = 0;
@@ -23,14 +23,14 @@ var t;
     socket.binaryType = "arraybuffer";
 
     // TPS logging
-    setInterval(function () {
-        if (settings.log_tps) {
-            var time = Date.now() - tps_last;
-            console.log("TPS [%f]: %d", time / 1000, tps);
-            tps_last = Date.now();
-            tps = 0;
-        }
-    }, 1000);
+    //setInterval(function () {
+    //    if (settings.log_tps) {
+    //        var time = Date.now() - tps_last;
+    //        console.log("TPS [%f]: %d", time / 1000, tps);
+    //        tps_last = Date.now();
+    //        tps = 0;
+
+    //}, 1000);
 
     function send(msg) {
         if (socket.readyState == WebSocket.OPEN) {
@@ -48,17 +48,63 @@ var t;
     };
 
     socket.onmessage = function (evt) {
-        //try {
+        try {
 
             var msg = protoroot.Message.decode(evt.data),
                 which = msg.Payload;
             if (which == null) throw "ERROR";
             var tick = 0;
-            t = msg.Tick;
             switch (which) {
                 case 'GameState':
                     console.log(msg.GameState);
                     serverInfo = msg.GameState.ServerInfo;
+                    var t = msg.SyncTick;
+                    var objStatesLength = msg.GameState.ObjStates.length;
+                    for(var k = 0; k < objStatesLength; k++) {
+                        var newObjState = msg.GameState.ObjStates[k];
+                        var newObj = new GameObj(newObjState.ObjId,
+                                                 newObjState.ObjState.XPos,
+                                                 newObjState.ObjState.YPos,
+                                                 newObjState.ObjState.Size,
+                                                 newObjState.ObjState.Velocity,
+                                                 newObjState.ObjState.Azimuth,
+                                                 newObjState.ObjType,
+                                                 newObjState.Description,
+                                                 newObjState.ObjTheme,
+                                                 newObjState.LastUpdatedTick);
+                        tree.insert(newObj);
+                    }
+
+                    var initState = new protoroot.GameObjState({
+                        XPos: 5,
+                        YPos: 5,
+                        Size: 5,
+                        Velocity: 0.5,
+                        Azimuth: .7
+                    });
+
+                    var createdObj = new protoroot.CreateObjectPayload({
+                        ObjType: "player",
+                        ObjTheme:  Math.floor(Math.random() * 16777215).toString(16),
+                        Description: "player1",
+                        InitialState: initState
+                    });
+
+                    var gmEvent = new protoroot.GameEvent({
+                        Tick: t+1,
+                        InitObjId: 0,
+                        TargetObjId: 0,
+                        CreateObject: createdObj
+                    });
+
+                    var message = new protoroot.Message({
+                        GameEvent: gmEvent
+                    });
+                    var new_msg = message.encode();
+                    send(new_msg);
+
+
+
                     break;
 
                 case 'GameHeartbeat':
@@ -73,8 +119,8 @@ var t;
                             case 'ObjectAbsorbedPayload':
 
                                 var obj = {
-                                    id: event.TargetObjId,
-                                }
+                                    id: event.TargetObjId
+                                };
 
                                 //update to do animation!
                                 tree.remove(obj);
@@ -120,11 +166,11 @@ var t;
                             node.azimuth = obj.azimuth;
                         }
                     }
-                    tick = msg.Tick;
                     //console.log(obj);
                     break;
 
                 case 'GameEvent':
+                    console.log("yee");
                     var event = msg.GameEvent;
                     main = new GameObj(
                         event.TargetObjId,
@@ -150,21 +196,21 @@ var t;
                 main.velocity,
                 main.azimuth
             );
-            var ObjUpdate = new protoroot.GameObjUpdate(
-                main.id,
-                ObjState
-            );
+            var ObjUpdate = new protoroot.GameObjUpdate({
+                Tick: t+1,
+                ObjId: main.id,
+                ObjState: ObjState
+            });
 
             var message = new protoroot.Message({
-                Tick: t,
                 GameObjUpdate: ObjUpdate
             });
             var new_msg = message.encode();
-            //send(new_msg);
+            send(new_msg);
 
-        //} //catch (err) {
-          //  console.log(err);
-        //}
+        } catch (err) {
+            console.log(err);
+        }
 
     };
 })();
